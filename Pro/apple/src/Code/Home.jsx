14a11e95a6555488686026d12dc173bb9406e3ba
abhishek 1199 <abhishekshaw1199@gmail.com>
2023-08-19 23:09:27 +0530

    4

--- Pro/apple/src/Code/Home.jsx
+++ Pro/apple/src/Code/Home.jsx
@@ -5,181 +5,265 @@ import Footer from "./Footer";
 
 
 
-const Home = ({timeline}) => {
-    
+const Home = ({ timeline }) => {
+
     return (
         <div>
             <div className='mac'
-          
-          >
-                <h1  data-aos="zoom-in"  data-aos-delay="1400" data-aos-duration="2700"
-                 >MacBook Air 15''</h1>
+
+            >
+                <h1 data-aos="zoom-in" data-aos-delay="1400" data-aos-duration="2700"
+                >MacBook Air 15''</h1>
                 <h2 data-aos="zoom-out-up" data-aos-delay="1400" data-aos-duration="3000"
-                 >Impressively big. Impossibly thin.</h2>
+                >Impressively big. Impossibly thin.</h2>
                 <div className='macp' data-aos="fade-up" data-aos-delay="1400" data-aos-duration="1500">
                     <p data-aos="fade-down-right" data-aos-delay="1400" data-aos-duration="2700"><a href="" >Learn more <AiOutlineRight /> </a></p>
                     <p data-aos="fade-up-left" data-aos-delay="1400" data-aos-duration="2700"><a href="" >Buy <AiOutlineRight /> </a></p>
                 </div>
 
                 <div className='image-content'
-                data-aos="flip-left"
-            
-                data-aos-easing="ease-out-cubic"
-                data-aos-duration="3000">
+                    data-aos="flip-left"
+
+                    data-aos-easing="ease-out-cubic"
+                    data-aos-duration="3000">
                     <img src={require('./Image/apple laptop.jpg')} alt="err" />
                 </div>
 
             </div>
 
             <div className='ip14pro'
-             data-aos="fade-down-left"
-             data-aos-duration="1500"
-             data-aos-mirror="true"
-             data-aos-once="false"
-             data-aos-anchor-placement="top-center"
+                data-aos="fade-down-left"
+                data-aos-duration="1500"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-center"
             >
                 <h1 data-aos="flip-up"
-                data-aos-delay="200"
-                data-aos-easing="ease-in-out-sine"
-             data-aos-duration="2100"
-             data-aos-mirror="true"
-             data-aos-once="false"
-             data-aos-anchor-placement="top-center"
-             > iPhone 14 Pro</h1>
+                    data-aos-delay="200"
+                    data-aos-easing="ease-in-out-sine"
+                    data-aos-duration="2100"
+                    data-aos-mirror="true"
+                    data-aos-once="false"
+                    data-aos-anchor-placement="top-center"
+                > iPhone 14 Pro</h1>
 
-                <h2  data-aos="zoom-out-right"
-                data-aos-delay="300"
-                data-aos-easing="ease-in-sine"
-             data-aos-duration="2400"
-            data-aos-mirror="true"
-             data-aos-once="false"
-             data-aos-anchor-placement="top-center"
-             >Pro.Beyond</h2>
+                <h2 data-aos="zoom-out-right"
+                    data-aos-delay="300"
+                    data-aos-easing="ease-in-sine"
+                    data-aos-duration="2400"
+                    data-aos-mirror="true"
+                    data-aos-once="false"
+                    data-aos-anchor-placement="top-center"
+                >Pro.Beyond</h2>
 
                 <div className="i14p" >
-                    <p  data-aos="flip-down"
-                    data-aos-easing="ease-in-back"
-                    data-aos-delay="400"
-             data-aos-duration="2700"
-            data-aos-mirror="true"
-             data-aos-once="false"><a href="">Learn more <AiOutlineRight /> </a></p>
-                    <p  data-aos="flip-left"
-                    data-aos-easing="ease-in-sine"
-                    data-aos-delay="400"
-             data-aos-duration="2700"
-            data-aos-mirror="true"
-             data-aos-once="false"><a href="">Buy <AiOutlineRight /> </a></p>
+                    <p data-aos="flip-down"
+                        data-aos-easing="ease-in-back"
+                        data-aos-delay="400"
+                        data-aos-duration="2700"
+                        data-aos-mirror="true"
+                        data-aos-once="false"><a href="">Learn more <AiOutlineRight /> </a></p>
+                    <p data-aos="flip-left"
+                        data-aos-easing="ease-in-sine"
+                        data-aos-delay="400"
+                        data-aos-duration="2700"
+                        data-aos-mirror="true"
+                        data-aos-once="false"><a href="">Buy <AiOutlineRight /> </a></p>
                 </div>
 
                 <div className='image-content' data-aos="zoom-out-up"
-                data-aos-easing="ease-in-out-back"
-                data-aos-delay="100"
-             data-aos-duration="3000"
-            data-aos-mirror="true"
-             data-aos-once="false">
-                    <img  src="https://photos5.appleinsider.com/gallery/50473-99283-iPhone-14-Pro-Max-colors-xl.jpg" alt="err" />
+                    data-aos-easing="ease-in-out-back"
+                    data-aos-delay="100"
+                    data-aos-duration="3000"
+                    data-aos-mirror="true"
+                    data-aos-once="false">
+                    <img src="https://photos5.appleinsider.com/gallery/50473-99283-iPhone-14-Pro-Max-colors-xl.jpg" alt="err" />
                 </div>
             </div>
 
 
             <div className='ip14'>
-                <h1  data-aos="zoom-out-up"
-                data-aos-delay="600"
-                data-aos-easing="ease-out-sine"
-             data-aos-duration="3000"
-            data-aos-mirror="true"
-             data-aos-once="false" data-aos-anchor-placement="top-center"> iPhone 14 </h1>
-                <h2   data-aos="zoom-out-down"
-                data-aos-delay="400"
-                data-aos-easing="ease-out-sine"
-             data-aos-duration="3000"
-            data-aos-mirror="true"
-             data-aos-once="false" data-aos-anchor-placement="top-center">Wonderful.</h2>
+                <h1 data-aos="zoom-out-up"
+                    data-aos-delay="600"
+                    data-aos-easing="ease-out-sine"
+                    data-aos-duration="3000"
+                    data-aos-mirror="true"
+                    data-aos-once="false" data-aos-anchor-placement="top-center"> iPhone 14 </h1>
+                <h2 data-aos="slide-right"
+                    data-aos-delay="400"
+                    data-aos-easing="ease-out-sine"
+                    data-aos-duration="3000"
+                    data-aos-mirror="true"
+                    data-aos-once="false" data-aos-anchor-placement="top-center">Wonderful.</h2>
                 <div className="ip14p">
-                    <p data-aos="zoom-out-down"
-                data-aos-delay="200"
-                data-aos-easing="ease-out-sine"
-             data-aos-duration="3000"
-            data-aos-mirror="true"
-             data-aos-once="false" data-aos-anchor-placement="top-center"><a href="">Learn more <AiOutlineRight /> </a></p>
-                    <p data-aos="zoom-out-down"
-                data-aos-delay="200"
-                data-aos-easing="ease-out-sine"
-             data-aos-duration="3000"
-            data-aos-mirror="true"
-             data-aos-once="false" data-aos-anchor-placement="top-center"><a href="">Buy <AiOutlineRight /> </a></p>
+                    <p data-aos="zoom-out-right"
+                        data-aos-delay="200"
+                        data-aos-easing="ease-out-sine"
+                        data-aos-duration="3000"
+                        data-aos-mirror="true"
+                        data-aos-once="false" data-aos-anchor-placement="top-center"><a href="">Learn more <AiOutlineRight /> </a></p>
+                    <p data-aos="zoom-out-left"
+                        data-aos-delay="200"
+                        data-aos-easing="ease-out-sine"
+                        data-aos-duration="3000"
+                        data-aos-mirror="true"
+                        data-aos-once="false" data-aos-anchor-placement="top-center"><a href="">Buy <AiOutlineRight /> </a></p>
                 </div>
 
-                <div className='image-content' data-aos="zoom-out-down"
-                data-aos-delay="100"
-                data-aos-easing="ease-out-sine"
-             data-aos-duration="3000"
-            data-aos-mirror="true"
-             data-aos-once="false" data-aos-anchor-placement="top-center">
+                <div className='image-content' data-aos="zoom-in-right"
+                    data-aos-delay="100"
+                    data-aos-easing="ease-in-out-sine"
+                    data-aos-duration="3000"
+                    data-aos-mirror="true"
+                    data-aos-once="false" data-aos-anchor-placement="top-center">
                     <img src="https://www.macobserver.com/wp-content/uploads/2022/09/Apple-iPhone-14-Lineup-1024x538.jpg" alt="err" />
                 </div>
             </div>
-            <div className='school'>
-                <div>
-                    <h1>
-                        It's easy to switch from <br />
-                        Android to iPhone.
+            <div className='school' data-aos="zoom-out-left"
+                data-aos-delay="1400"
+                data-aos-easing="ease-out-sine"
+                data-aos-duration="3000"
+                data-aos-mirror="true"
+                data-aos-once="false" data-aos-anchor-placement="top-center">
+
+                <div data-aos="slide-left"
+                data-aos-delay="1000"
+                data-aos-easing="ease-out-sine"
+                data-aos-duration="1500"
+                data-aos-mirror="true"
+                data-aos-once="false" data-aos-anchor-placement="top-center">
+                    <h1 className="schooltext" data-aos="fade-up-left"
+                              data-aos-delay="800"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="1800"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center" >
+                        It's easy to switch from
+
+
                     </h1>
-                    <p>Isha easily transferred data to a new iPhone</p>
-                    <div >
-                        <p><a href="">Watch her film <AiFillPlayCircle /> </a></p>
-                        <p><a href="">Discover more stories <AiOutlineRight /> </a></p>
-                        <div>
-                            <img src={require('./Image/sticker.png')} alt="err" />
-                        </div>
+                    <h1 className="schooltext" data-aos="fade-up-left"
+                              data-aos-delay="800"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="1800"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center" >Android to iPhone.</h1>
+                    <p className="schoolpara"  data-aos="fade-down-left"
+                              data-aos-delay="600"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="2100"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center">Isha easily transferred data to a new iPhone</p>
+                    <div className="markline" >
+                        <p data-aos="flip-left"
+                              data-aos-delay="400"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="2400"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center"><a href="">Watch her film <AiFillPlayCircle /> </a></p>
+                        <p data-aos="flip-left"
+                              data-aos-delay="400"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="2400"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center"><a href="">Discover more stories <AiOutlineRight /> </a></p>
                     </div>
+                    <div className="sticker"  data-aos="zoom-out-left"
+                              data-aos-delay="200"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="2700"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center">
+                        <img src={require('./Image/sticker.png')} alt="err" />
+                    </div>
+
                 </div>
-                <div>
-                    <h1>
-                        Save on Mac or iPad for<br />
+                <div className="save" data-aos="slide-right"
+                data-aos-delay="1000"
+                data-aos-easing="ease-out-sine"
+                data-aos-duration="1500"
+                data-aos-mirror="true"
+                data-aos-once="false" data-aos-anchor-placement="top-center">
+                    <h1 className="schooltext"
+                              data-aos="fade-up-right"
+                              data-aos-delay="800"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="1800"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center"          >
+                        Save on Mac or iPad for
+
+                    </h1>
+                    <h1 className="schooltext"  data-aos="fade-up-right"
+                              data-aos-delay="800"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="1800"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center" >
+
                         university.
                     </h1>
-                    <p>Plug get AirPods with Mac and Apple Pencil with iPad.</p>
+
+
+                    <p className="schoolpara"  data-aos="fade-down-right"
+                              data-aos-delay="600"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="2100"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center" >Plug get AirPods with Mac and Apple Pencil with iPad.</p>
                     <div >
 
-                        <p><a href="">Shop now <AiOutlineRight /> </a></p>
-                        <div>
+                        <p className="ip14p"  data-aos="flip-right"
+                              data-aos-delay="400"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="2400"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center" ><a href="">Shop now <AiOutlineRight /> </a></p>
+                        <div className="sticker"  data-aos="zoom-out-right"
+                              data-aos-delay="200"
+                              data-aos-easing="ease-out-sine"
+                              data-aos-duration="2700"
+                              data-aos-mirror="true"
+                              data-aos-once="false" data-aos-anchor-placement="top-center" >
                             <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/preunidays-header-hero-202306_GEO_IN?wid=402&hei=364&fmt=png-alpha&.v=1685548520689" alt="err" />
                         </div>
                     </div>
                 </div>
 
             </div>
-
-            <div className='padwatch'>
-                <div className='watch'>
-                    <h1>
-                        <AiFillApple />WATCH
-                    </h1>
-                    <p>SERIES 8</p>
-                    <h4>A healthy leap ahead.</h4>
-                    <div >
-                        <p><a href="">Learn more<AiOutlineRight /> </a></p>
-                        <p><a href="">Buy<AiOutlineRight /> </a></p>
-                        <div>
+            <div className='school'>
+                <div className='padwatch'>
+                    <div  className="pad1">
+                        <h1 className="watch">
+                            <AiFillApple />WATCH
+                        </h1>
+                        <p className="series">SERIES 8</p>
+                        <h4 className="tagh4">A healthy leap ahead.</h4>
+                        <div className="markline">
+                            <p><a href="">Learn more<AiOutlineRight /> </a></p>
+                            <p><a href="">Buy<AiOutlineRight /> </a></p>
+                        </div>
+                        <div className="sticker">
                             <img src={require('./Image/watch.png')} alt="err" />
                         </div>
+
                     </div>
-                </div>
-                <div>
-                    <h1>
-                        iPad Pro
-                    </h1>
-                    <h4>SuperCharged  by <AiFillApple />M2 </h4>
-                    <div >
-                        <p><a href="">Learn more<AiOutlineRight /> </a></p>
-                        <p><a href="">Buy<AiOutlineRight /> </a></p>
-                        <div>
+                    <div className="pad2">
+                        <h1 className="watch">
+                            iPad Pro
+                        </h1>
+                        <h4 className="tagh4">SuperCharged  by <AiFillApple />M2 </h4>
+                        <div className="markline">
+                            <p><a href="">Learn more<AiOutlineRight /> </a></p>
+                            <p><a href="">Buy<AiOutlineRight /> </a></p>
+                        </div>
+                        <div className="sticker">
                             <img src={require('./Image/ipad.png')} alt="err" />
                         </div>
-                    </div>
-                </div>
 
+                    </div>
+
+                </div>
             </div>
             <div className='trade'>
                 <div className='upgrade'>
@@ -214,7 +298,7 @@ const Home = ({timeline}) => {
 
 
 
-            <Footer/>
+            <Footer />
         </div>
     )
 }
